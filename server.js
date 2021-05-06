ae72b06f1394b1debfc10196a57f7be39d4a3278
sandeep <babasolution.it@gmail.com>
2021-05-07 01:32:47 +0530

    #welcome

---
+++ server.js
@@ -0,0 +1,42 @@
+// Requires "request" to be installed (see https://www.npmjs.com/package/request)
+var request = require('request');
+var fs = require('fs');
+var express = require("express");
+var bodyParser = require('body-parser');
+let path = require('path');
+let cors = require('cors');
+var app = express();
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+
+const port = 4001;
+const server = require('http').createServer(app);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+// # https://www.remove.bg/example.jpg
+app.post("/api/remove-bg",function(req,res){
+    console.log(req.body.imageUrl);
+    request.post({
+        url: 'https://api.remove.bg/v1.0/removebg',
+        formData: {
+          image_url: req.body.imageUrl,
+          size: 'auto',
+        },
+        headers: {
+          'X-Api-Key': '9EdDasvh5GFn8UFUb7h9dpLD'
+        },
+        encoding: null
+      }, function(error, response, body) {
+        if(error) return console.error('Request failed:', error);
+        if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+        console.log(body);
+        fs.writeFileSync("public/nobg.png", body);
+        return res.send({msg:"Removed Successfully","imageNBPath":"nobg.png"});
+      });    
+    
+});
+server.listen(port, () => {
+    console.log('Server listening on port: ', port);
+}).timeout = 1800000; //30 min
